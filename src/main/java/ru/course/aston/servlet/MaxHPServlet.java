1a1ef9b9f0bfbfd0e8b3d63347036df0d2d8b53a
Dionork <rodya.ru@inbox.ru>
2024-09-24 21:13:31 +0700

    Каркас Сервлетов

--- src/main/java/ru/course/aston/servlet/MaxHPServlet.java
+++ src/main/java/ru/course/aston/servlet/MaxHPServlet.java
@@ -7,29 +7,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-@WebServlet("/role/*")
-public class RoleServlet extends HttpServlet {
-    @Override
-    //Запрос на сервлет
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+@WebServlet("/MaxHPServlet/*")
+public class MaxHPServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
     }
 
     @Override
-    //Изменение объекта
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
     }
 
     @Override
-    //Новый объект
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPut(req, resp);
     }
 
     @Override
-    //Удаление объекта
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doDelete(req, resp);
     }
